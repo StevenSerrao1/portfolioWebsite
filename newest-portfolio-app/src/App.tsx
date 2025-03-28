e6bbc069f48db3ce8f50907836c36de116e3e281
Steven Serrao <stevenserraowork@gmail.com>
2025-03-28 11:21:33 +0200

    fuck tailwind, swapped to bootstrap

---
+++ newest-portfolio-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="container">
+        <h1 className="mt-5">Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Click Me</button>
+      </div>
+  );
+}
+
+export default App;
